c239766f0d6ae2916fb1253dd3322766ca81cc73
Fahim Fiyaz <faiyazfahim743@gmail.com>
2025-01-21 22:14:45 +0600

    Forget password verify email screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/UI/Screens/forgot_passowrd_verify_email.dart';
 import 'package:task_manager/UI/Screens/sign_in_screen.dart';
 import 'package:task_manager/UI/Screens/sign_up_screen.dart';
 import 'package:task_manager/UI/Screens/splash_screen.dart';
@@ -45,6 +46,8 @@ class TaskManager extends StatelessWidget {
             widget = const SignInScreen();
           } else if(settings.name==SignUpScreen.name){
             widget=const SignUpScreen();
+          } else if(settings.name==ForgotPasswordVerifyEmailScreen.name){
+            widget=const ForgotPasswordVerifyEmailScreen();
           }
           return MaterialPageRoute(builder: (_) => widget);
         });
