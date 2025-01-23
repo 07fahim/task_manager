09c33b9c80fac07a204ceef1e0259f6fa15f08ec
Fahim Fiyaz <faiyazfahim743@gmail.com>
2025-01-24 04:09:44 +0600

    updated profile screen

--- lib/UI/Screens/update_profile_screen.dart
+++ lib/UI/Screens/update_profile_screen.dart
@@ -5,7 +5,7 @@ import 'package:task_manager/UI/Widgets/tm_app_bar.dart';
 class UpdateProfileScreen extends StatefulWidget {
   const UpdateProfileScreen({super.key});
 
-  static const String name='/update-profile';
+  static const String name = '/update-profile';
 
   @override
   State<UpdateProfileScreen> createState() => _UpdateProfileScreenState();
@@ -13,80 +13,117 @@ class UpdateProfileScreen extends StatefulWidget {
 
 class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
   final TextEditingController _emailTextController = TextEditingController();
-  final TextEditingController _firstNameTextController = TextEditingController();
+  final TextEditingController _firstNameTextController =
+      TextEditingController();
   final TextEditingController _lastNameTextController = TextEditingController();
   final TextEditingController _mobileTextController = TextEditingController();
   final TextEditingController _passTextController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return  Scaffold(
+    return Scaffold(
       appBar: const TMAppBar(
         fromUpdateProfile: true,
       ),
       body: ScreenBackground(
         child: Form(
-          child: Padding(
-            padding: const EdgeInsets.all(24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              key: _formKey,
-              children: [
-                const SizedBox(height:48),
-                Text(
-                  'Update Profile',
-                  style: textTheme.titleLarge,
-                ),
-                const SizedBox(height: 24),
-                TextFormField(
-                  controller: _emailTextController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(hintText: "Email"),
-                ),
-                const SizedBox(height: 8),
-                TextFormField(
-                  controller: _firstNameTextController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "First Name",
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                key: _formKey,
+                children: [
+                  const SizedBox(height: 40),
+                  Text(
+                    'Update Profile',
+                    style: textTheme.titleLarge,
                   ),
-                ),
-                const SizedBox(height: 8),
-                TextFormField(
-                  controller: _lastNameTextController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "Last Name",
+                  const SizedBox(height: 24),
+                  _buildPhotoPicker(),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _emailTextController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(hintText: "Email"),
                   ),
-                ),
-                const SizedBox(height: 8),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  controller: _mobileTextController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "Mobile",
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _firstNameTextController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "First Name",
+                    ),
                   ),
-                ),
-                const SizedBox(height: 8),
-                TextFormField(
-                  controller: _passTextController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "Password",
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _lastNameTextController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Last Name",
+                    ),
                   ),
-                ),
-                const SizedBox(height: 24),
-                ElevatedButton(
-                    onPressed: () {},
-                    child: const Icon(Icons.arrow_circle_right,size: 30,)),
-
-              ],
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    controller: _mobileTextController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Mobile",
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _passTextController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Password",
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: const Icon(
+                        Icons.arrow_circle_right,
+                        size: 30,
+                      )),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildPhotoPicker() {
+    return Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8)),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 50,
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      decoration: const BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(8),
+                              bottomLeft: Radius.circular(8))),
+                      alignment: Alignment.center,
+                      child: const Text(
+                        "Photo",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    const SizedBox(width: 12,),
+                    const Text("No item selected",maxLines: 1,)
+                  ],
+                ),
+              );
   }
 }
