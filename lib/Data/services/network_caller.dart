29742e010d1e1e1ac6010e9239b5fe9e5c976da7
Fahim Fiyaz <faiyazfahim743@gmail.com>
2025-01-25 15:43:13 +0600

    Api

--- lib/Data/services/network_caller.dart
+++ lib/Data/services/network_caller.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
 
@@ -7,12 +9,32 @@ class NetworkResponse {
   final bool isSuccess;
   final String? errorMessage;
 
-  NetworkResponse({
-    required this.statusCode,
-    required this.isSuccess,
-    this.errorMessage,
-    this.responseData
-  });
+  NetworkResponse(
+      {required this.statusCode,
+      required this.isSuccess,
+      this.errorMessage,
+      this.responseData});
 }
 
-class NetworkCaller {}
+class NetworkCaller {
+  static Future<NetworkResponse> getRequest(
+      {required String url, required Map<String, dynamic>? params}) async {
+    Uri uri = Uri.parse(url);
+    debugPrint("URL => $url");
+    Response response = await get(uri);
+    debugPrint("Response code => ${response.statusCode}");
+    debugPrint("Response code => ${response.body}");
+
+    if (response.statusCode == 200) {
+      final decodedResponse = jsonDecode(response.body);
+      return NetworkResponse(
+          statusCode: response.statusCode,
+          isSuccess: true,
+          responseData: decodedResponse);
+    } else {
+      return NetworkResponse(
+          statusCode: response.statusCode,
+          isSuccess: false);
+    }
+  }
+}
