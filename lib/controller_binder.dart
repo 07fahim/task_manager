8624d8fa2530645a185c1beaa04746f2db3db4c4
Fahim Fiyaz <faiyazfahim743@gmail.com>
2025-02-13 19:47:54 +0600

    getx controller binder

--- lib/controller_binder.dart
+++ lib/controller_binder.dart
@@ -1,13 +1,23 @@
 import 'package:get/get.dart';
+import 'package:task_manager/UI/controller/auth_controller.dart';
 import 'package:task_manager/UI/controller/sign_in_controller.dart';
+import 'package:task_manager/UI/controller/sign_up_controller.dart';
+import 'package:task_manager/UI/controller/forgot_password_email_controller.dart';
+import 'package:task_manager/UI/controller/forgot_password_otp_controller.dart';
+import 'package:task_manager/UI/controller/reset_password_controller.dart';
+import 'package:task_manager/UI/controller/update_profile_controller.dart';
 
-
-class ControllerBinder extends Bindings{
-
+class ControllerBinder extends Bindings {
   @override
-  void dependencies(){
+  void dependencies() {
+    // Put auth controller first as other controllers might depend on it
+    Get.put(AuthController(), permanent: true);
+
     Get.lazyPut(() => SignInController());
-
-
+    Get.lazyPut(() => SignUpController());
+    Get.lazyPut(() => ForgotPasswordEmailController());
+    Get.lazyPut(() => ForgotPasswordOTPController());
+    Get.lazyPut(() => ResetPasswordController());
+    Get.lazyPut(() => UpdateProfileController());
   }
-  }
+}
