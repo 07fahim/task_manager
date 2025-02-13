31a46c9d3fb6b9483932dba6e8f9f051e1f2a1d8
Fahim Fiyaz <faiyazfahim743@gmail.com>
2025-02-13 19:43:33 +0600

    auth controller with GetX

--- lib/UI/controller/auth_controller.dart
+++ lib/UI/controller/auth_controller.dart
@@ -1,50 +1,111 @@
 import 'dart:convert';
-
+import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:task_manager/Data/models/user_model.dart';
+import 'package:task_manager/data/models/user_model.dart';
 
-class AuthController {
-  static String? accessToken;
-  static UserModel? userModel;
+class AuthController extends GetxController {
+  static AuthController get instance => Get.find<AuthController>();
+
+  final Rx<UserModel?> _userModel = Rx<UserModel?>(null);
+  final RxString _accessToken = ''.obs;
+  final RxBool _isLoading = false.obs;
+
+  UserModel? get userModel => _userModel.value;
+  String get accessToken => _accessToken.value;
+  bool get isLoading => _isLoading.value;
 
   static const String _accessTokenKey = 'access-token';
   static const String _userDataKey = 'user-data';
 
-  static Future<void> saveUserData(String accessToken, UserModel model) async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    await sharedPreferences.setString(_accessTokenKey, accessToken);
-    await sharedPreferences.setString(_userDataKey, jsonEncode(model.toJson()));
-    userModel = model;
-    AuthController.accessToken = accessToken;
+  @override
+  void onInit() {
+    super.onInit();
+    isUserLoggedIn();
   }
 
-  static Future<void> getUserData() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    String? token = sharedPreferences.getString(_accessTokenKey);
-    String? userData = sharedPreferences.getString(_userDataKey);
-    accessToken = token;
-    userModel = UserModel.fromJson(jsonDecode(userData!));
-  }
+  Future<void> saveUserData(String token, UserModel model) async {
+    try {
+      _isLoading.value = true;
 
-  static Future<void> updateUserData(UserModel model) async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    await sharedPreferences.setString(_userDataKey, jsonEncode(model.toJson()));
-    userModel = model;
-  }
+      SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
 
-  static Future<bool> isUserLoggedIn() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    String? token = sharedPreferences.getString(_accessTokenKey);
-    if (token != null) {
-      await getUserData();
-      return true;
+      await sharedPreferences.setString(_accessTokenKey, token);
+      await sharedPreferences.setString(_userDataKey, jsonEncode(model.toJson()));
+
+      _userModel.value = model;
+      _accessToken.value = token;
+    } catch (e) {
+      print('Error saving user data: $e');
+    } finally {
+      _isLoading.value = false;
     }
-    return false;
   }
 
-  static Future<void> clearUserData() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    await sharedPreferences.clear();
+  Future<void> getUserData() async {
+    try {
+      _isLoading.value = true;
+
+      SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+      String? token = sharedPreferences.getString(_accessTokenKey);
+      String? userData = sharedPreferences.getString(_userDataKey);
+
+      if (token != null && userData != null) {
+        _accessToken.value = token;
+        _userModel.value = UserModel.fromJson(jsonDecode(userData));
+      }
+    } catch (e) {
+      print('Error getting user data: $e');
+    } finally {
+      _isLoading.value = false;
+    }
   }
 
+  Future<void> updateUserData(UserModel model) async {
+    try {
+      _isLoading.value = true;
+
+      SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+      await sharedPreferences.setString(_userDataKey, jsonEncode(model.toJson()));
+
+      _userModel.value = model;
+    } catch (e) {
+      print('Error updating user data: $e');
+    } finally {
+      _isLoading.value = false;
+    }
+  }
+
+  Future<bool> isUserLoggedIn() async {
+    try {
+      SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+      String? token = sharedPreferences.getString(_accessTokenKey);
+
+      if (token != null) {
+        await getUserData();
+        return true;
+      }
+      return false;
+    } catch (e) {
+      print('Error checking login status: $e');
+      return false;
+    }
+  }
+
+  Future<void> clearUserData() async {
+    try {
+      _isLoading.value = true;
+
+      SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+      await sharedPreferences.clear();
+
+      _userModel.value = null;
+      _accessToken.value = '';
+    } catch (e) {
+      print('Error clearing user data: $e');
+    } finally {
+      _isLoading.value = false;
+    }
+  }
 }
